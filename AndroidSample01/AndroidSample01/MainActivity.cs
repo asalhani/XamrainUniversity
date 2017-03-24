899e398ae1d8e5693ca017dd9b7047d54112358d
Adib Ahmad <asalhani@outlook.com>
2017-03-24 09:33:38 +0300

    intial commit

---
+++ AndroidSample01/AndroidSample01/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace AndroidSample01
+{
+    [Activity(Label = "Android Sample - 01", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button btnCalculate;
+        EditText txtBill;
+        TextView lblTip;
+        TextView lblTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            txtBill = FindViewById<EditText>(Resource.Id.txtBill);
+            lblTip = FindViewById<TextView>(Resource.Id.lblTip);
+            lblTotal = FindViewById<TextView>(Resource.Id.lblTotal);
+            btnCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
+
+            btnCalculate.Click += onCalculateClick;
+        }
+
+        void onCalculateClick (object sender, EventArgs e)
+        {
+            string strBill = txtBill.Text;
+            var bill = double.Parse(strBill);
+
+            double tip = bill * 0.15;
+            double total = bill + tip;
+
+            lblTip.Text = tip.ToString();
+            lblTotal.Text = total.ToString();
+        }
+    }
+}
+
---
+++ AndroidSample01/AndroidSample01/obj/Debug/android/src/md581228d65ab291fbee5c937fb5e4f9e58/MainActivity.java
@@ -0,0 +1,47 @@
+package md581228d65ab291fbee5c937fb5e4f9e58;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("AndroidSample01.MainActivity, AndroidSample01, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("AndroidSample01.MainActivity, AndroidSample01, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
